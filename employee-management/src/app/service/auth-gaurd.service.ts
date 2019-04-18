cdc111a07120fc2a8fc50cdd795213882f7826a9
depauna <natan.depauw@flowfactor.be>
2019-04-19 00:32:17 +0200

    CRUD + basic authentication

---
+++ employee-management/src/app/service/auth-gaurd.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router/src/utils/preactivation';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGaurdService implements CanActivate {
+  path: ActivatedRouteSnapshot[];
+  route: ActivatedRouteSnapshot;
+
+  constructor(private router: Router,
+    private authService: AuthenticationService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isLoggedIn())
+      return true;
+    
+    this.router.navigate(['login']);
+    return false;
+  }
+}
---
+++ employee-management/src/app/service/authentication.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+
+  authenticate(username, password) {
+    if (username === "natan" && password === "hello") {
+      sessionStorage.setItem('username', username)
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isLoggedIn() {
+    let user = sessionStorage.getItem('username')
+    console.log(!(user === null))
+    return !(user === null)
+  }
+
+  logOut() {
+    sessionStorage.removeItem('username')
+  }
+}
