b5199d7989dadfa8c5fc9e613a0e1c3d75d20607
depauna <natan.depauw@flowfactor.be>
2019-05-07 11:24:07 +0200

    changed apiUrl

--- employee-management/src/environments/environment.prod.ts
+++ employee-management/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://employee-management-backend-service:8080'
-  // apiUrl: 'http://137.135.173.235:30271'
+  apiUrl: 'http://backend.mycluster'
 };
